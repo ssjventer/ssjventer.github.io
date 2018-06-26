70e81a51fa660fde591ba744178957e1fde25b34
Stuart Venter <stuart@entelect.co.za>
2018-06-26 16:30:00 +0200

    Major Changes

--- scripts/main.js
+++ scripts/main.js
@@ -18,14 +18,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
     var myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'HTML + CSS + JS, ' + myName;
+    myHeading.textContent = myName;
 }
 
 if(!localStorage.getItem('name')) {
     setUserName();
   } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'HTML + CSS + JS, ' + storedName;
+    myHeading.textContent = storedName;
   }
 
   myButton.onclick = function() {
